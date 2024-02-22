0a97e5a0a207ff506a67f799560b9ca9db1c0dcf
PrincyAkash <160757262+PrincyAkash@users.noreply.github.com>
2024-02-22 19:27:54 +0530

    Sum of the elements array

---
+++ SumOfElements.java
@@ -0,0 +1,20 @@
+//Write a program, to sum up all the elements of an array
+
+
+class SumOfElements{
+
+public static void main(String args[]){
+
+
+int arr[] ={1,2,3,4,5};	
+int sum = 0;
+
+for(int i=0; i<5;i++){
+sum = sum + arr[i];
+}
+
+System.out.println(sum);
+
+}
+}
+
